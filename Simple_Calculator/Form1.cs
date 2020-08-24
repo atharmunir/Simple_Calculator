7fbfa91ac16c111deb994c5e39420a52da2535e1
Athar Munir <mamunir@gmail.com>
2020-08-24 00:16:06 -0600

    Addition Working

--- Simple_Calculator/Form1.cs
+++ Simple_Calculator/Form1.cs
@@ -15,5 +15,24 @@ namespace Simple_Calculator
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(textBox1.Text);
+            int num2 = int.Parse(textBox2.Text);
+
+            int result = num1 + num2;
+
+            //MessageBox.Show("Addition: " + result.ToString());
+
+            label4.Text = ("=  " + result.ToString());
+            label4.Visible = true;
+        }
+
     }
 }
